a6c56790a8b60d7307d1caae3635d638c8aaefb6
Edmund Tsang <69718180+edmund844328@users.noreply.github.com>
2022-10-29 22:46:53 +0800

    w

--- TRPO/TrpoRender.py
+++ TRPO/TrpoRender.py
@@ -1,6 +1,6 @@
 import torch as th
 import gym
-from tqdm import tqdm, trange
+from tqdm import trange
 from sb3_contrib import TRPO
 import matplotlib.pyplot as plt
 
@@ -12,17 +12,18 @@ else:
     print("Device set to : cpu")
 
 num_features = 128 # number of neurals per layers
-num_layers = 3
+num_layers = 2
 episodes = 1000
+act_f = th.nn.ReLU
 # Custom actor (pi) and value function (vf) networks
 # Custom MLP policy of three layers of size 128 each
-policy_kwargs = dict(activation_fn=th.nn.ReLU,
+policy_kwargs = dict(activation_fn=act_f,
                      net_arch=[dict(pi=[num_features]*num_layers, vf=[num_features]*num_layers)])
                                             
 env = gym.make('LunarLanderContinuous-v2')
 
 model = TRPO('MlpPolicy', env, policy_kwargs=policy_kwargs, gamma=0.99, target_kl=0.01, batch_size=64,verbose=1)
-model_name = "trpo_{}F_{}L_{}Episo_{}BatchS_{}γ_{}LearnR_{}TargetKL".format(num_features,num_layers, episodes,model.batch_size,model.gamma,model.learning_rate,model.target_kl)
+model_name = "trpo_{}F_{}L_{}Episo_{}BatchS_{}γ_{}LearnR_{}TargetKL_{}".format(num_features,num_layers, episodes,model.batch_size,model.gamma,model.learning_rate,model.target_kl,act_f.__name__)
 
 reward_per_episode = []
 
@@ -32,7 +33,7 @@ for episode in trange(episodes):
     state = env.reset()
     total_reward, total_step = 0, 0
     while True:
-        env.render()
+        # env.render()
         action, _state = model.predict(state)
         next_state, reward, done, _ = env.step(action)
         state = next_state
@@ -43,19 +44,9 @@ for episode in trange(episodes):
             reward_per_episode.append(total_reward)
             break
 
-env.close()
+# env.close()
+plt.axhline(y = 200, color = 'g', linestyle = '-')
+plt.axhline(y = 0, color = 'r', linestyle = '-')
 plt.plot(reward_per_episode)
-plt.title("Rewards per episode")
+plt.title("{}, Rewards per episode".format(model_name))
 plt.show()
-
-del model
-model = TRPO.load("trpo_lunar")
-while True:
-    env.render()
-    action, _state = model.predict(state)
-    next_state, reward, done, _ = env.step(action)
-    state = next_state
-    if done:
-        env.reset()
-
-
