834b6254ae09b35d3f1d65c8ae5d757a83920d17
Edmund Tsang <69718180+edmund844328@users.noreply.github.com>
2022-10-29 16:49:00 +0800

    a

--- TRPO/TrpoRender.py
+++ TRPO/TrpoRender.py
@@ -7,12 +7,13 @@ import matplotlib.pyplot as plt
 if(th.cuda.is_available()): 
     device = th.device('cuda:0') 
     th.cuda.empty_cache()
-    print("Device set to : " + str(torch.cuda.get_device_name(device)))
+    print("Device set to : " + str(th.cuda.get_device_name(device)))
 else:
     print("Device set to : cpu")
 
 num_features = 128 # number of neurals per layers
 num_layers = 3
+episodes = 1000
 # Custom actor (pi) and value function (vf) networks
 # Custom MLP policy of three layers of size 128 each
 policy_kwargs = dict(activation_fn=th.nn.ReLU,
@@ -21,12 +22,13 @@ policy_kwargs = dict(activation_fn=th.nn.ReLU,
 env = gym.make('LunarLanderContinuous-v2')
 
 model = TRPO('MlpPolicy', env, policy_kwargs=policy_kwargs, gamma=0.99, target_kl=0.01, batch_size=64,verbose=1)
+model_name = "trpo_{}F_{}L_{}Episo_{}BatchS_{}γ_{}LearnR_{}TargetKL".format(num_features,num_layers, episodes,model.batch_size,model.gamma,model.learning_rate,model.target_kl)
 
-episodes = 1
 reward_per_episode = []
 
 for episode in trange(episodes):
-    model = model.learn(total_timesteps=500, log_interval=4)
+    model.learn(total_timesteps=1000, log_interval=4)
+    model.save(model_name)
     state = env.reset()
     total_reward, total_step = 0, 0
     while True:
@@ -45,4 +47,15 @@ env.close()
 plt.plot(reward_per_episode)
 plt.title("Rewards per episode")
 plt.show()
+
+del model
+model = TRPO.load("trpo_lunar")
+while True:
+    env.render()
+    action, _state = model.predict(state)
+    next_state, reward, done, _ = env.step(action)
+    state = next_state
+    if done:
+        env.reset()
+
 
